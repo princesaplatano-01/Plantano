8bf2f9cab1cbe418d1561f5d41ad86138a39d6ff
Bambou Hocepied <bambouhocepied@gmail.com>
2026-03-10 16:19:20 -0600

    About: make subtitles bold and lighter grey

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,10 +13,10 @@ export default function AboutPage() {
           />
         </div>
         <div className="max-w-3xl mt-8 px-4">
-          <p className="text-lg md:text-xl italic text-[#bdbdbd] text-left">
+          <p className="text-lg md:text-xl italic font-semibold text-[#e6e6e6] text-left">
             Princesa Plátano is an ode to my inner child that's what I used to call myself.
           </p>
-          <p className="text-lg md:text-xl italic text-[#bdbdbd] text-right mt-4">
+          <p className="text-lg md:text-xl italic font-semibold text-[#e6e6e6] text-right mt-4">
             Now it's a brand that celebrates being different and unique.
           </p>
         </div>
